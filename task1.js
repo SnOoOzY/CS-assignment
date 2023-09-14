952e292e38d99032114768617420a42518b161c0
Lewis <lewis.kerr26@gmail.com>
2023-09-14 14:46:23 +0100

    t push

--- task1.js
+++ task1.js
@@ -2,7 +2,15 @@ const startMileage = prompt("Starting Mileage:  ");
 
 let Miles = [];
 let kW = [];
+let milediff = [];
 
 for (let i = 0; i < Stations; i++) {
+    let Stations = prompt+("How many stations visited: ");
+    let kwIn = prompt+("Kw at station:  ");
+    let mileStart = prompt+("Mileage at start: ");
+    let mileStation = prompt+("Mileage at station:  ");
+
+    kW.push(kwIn);
     
+
 }
